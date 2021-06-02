f1d68ab2923f34d68348293248c44523a5af0246
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-06-02 09:06:09 +0200

    changed a little the post card edit icon

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card , CardActions , CardContent , CardMedia , Button , Typography } from '@material-ui/core';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import EditICon from '@material-ui/icons/Edit';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
 
@@ -20,9 +20,9 @@ const Post = ({ post , setCurrentId }) => {
             <Typography variant="h6">{post.creator}</Typography>
             <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
           </div>
-          <div className={classes.overlay2}>
-            <Button style={{ color: 'white' }} size="small" onClick={() => setCurrentId(post._id)}><MoreHorizIcon fontSize="default" /></Button>
-          </div>
+          {/* <div className={classes.overlay2}>
+            <Button style={{ color: 'white' }} size="small" onClick={() => setCurrentId(post._id)}><EditICon fontSize="default" /></Button>
+          </div> */}
           <div className={classes.details}>
             <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
           </div>
@@ -31,8 +31,9 @@ const Post = ({ post , setCurrentId }) => {
             <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
           </CardContent>
           <CardActions className={classes.cardActions}>
-            <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" /> &nbsp; Like {post.likeCount} </Button>
+            {/* <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" /> &nbsp; Like {post.likeCount} </Button> */}
             <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}><DeleteIcon fontSize="small" /> Delete</Button>
+            <Button size="small" color="primary" onClick={() => setCurrentId(post._id)}><EditICon fontSize="default" />Edit</Button>
           </CardActions>
         </Card>
       );
